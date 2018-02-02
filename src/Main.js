2728c709daa448ad60f608d40316f10f1e350ca7
Kari Eronen <kari.eronen71@gmail.com>
2018-02-02 11:08:12 +0200

    logo

--- src/Main.js
+++ src/Main.js
@@ -10,31 +10,33 @@ import bigdata from "./bigdata";
 import fullstack from "./fullstack";
 import datamodeling from "./datamodeling";
 import contact from "./contact";
-import logo from '/home/reactjs/Reactjs_constreng/constreng/constreng/src/constrenglogo.png';
+import logoheader from "./logoheader";
  
 class Main extends Component {
   render() {
     return (
        <HashRouter>
         <div>
-<div className="logo">
-          <img src={logo} width="150" height="100" />
-        </div> 
+
 
          <h1>Constructive Engineering - Constreng</h1>
+	
+
           <ul className="header">
             <li><NavLink exact to="/">Summary</NavLink></li>
             <li><NavLink to="/bigdata"> Big data analysis</NavLink></li>
             <li><NavLink to="/fullstack">Full stack development</NavLink></li>
 	    <li><NavLink to="/datamodeling">Data modeling</NavLink></li>
 	    <li><NavLink to="/contact">Contact information</NavLink></li>
+	    <li><NavLink to="/logoheader">Constreng logo</NavLink></li>
           </ul>
           <div className="content">
             <Route exact path="/" component={Summary}/>
             <Route path="/bigdata" component={bigdata}/>
             <Route path="/fullstack" component={fullstack}/>
 	    <Route path="/datamodeling" component={datamodeling}/>
-            <Route path="/contact" component={contact}/>             
+            <Route path="/contact" component={contact}/>
+	    <Route path="/logoheader" component={logoheader}/>
           </div>
         </div>
       </HashRouter>
---
+++ src/logoheader.js
@@ -0,0 +1,20 @@
+
+import React, { Component } from "react";
+import constrenglogo from '/home/reactjs/Reactjs_constreng/constreng/constreng/src/constrenglogo.png';
+
+ 
+class logoheader extends Component {
+  render() {
+    return (
+      <div>
+<div className="constrenglogo">
+          <img src={constrenglogo} width="600" height="400" />
+        </div>
+
+        
+      </div>
+    );
+  }
+}
+ 
+export default logoheader;
